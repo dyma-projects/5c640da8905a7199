8b65195bf42d6f7a89e75a55fd6b0c0810dca302
Sylvie PERRIERE <calypso@MacBook-Pro-de-Caly.local>
2019-03-03 16:02:42 +0100

    ex1

---
+++ src/app/exercice-1/color.directive.ts
@@ -0,0 +1,6 @@
+import { Directive } from '@angular/core';
+
+@Directive({ selector: '[colorCustom]'})
+export class ColorDirective {
+  constructor() { }
+}
--- src/app/exercice-1/exercice-1.component.ts
+++ src/app/exercice-1/exercice-1.component.ts
@@ -6,14 +6,35 @@ import { Component, OnInit, HostListener } from '@angular/core';
 })
 export class Exercice1Component implements OnInit {
 
+  public newColor: string;
+
   // Ce Hostlistener permet d'écouter un événement sur l'objet window global;
   // Peut-être que cela vous sera utile pour votre directive ;)
   // (Il vous faudra bien sûr le déplacer dans celle-ci)
 
-  // @HostListener('window:click', ['$event']) windowClick($event) {
-  //   console.log($event);
-  // }
+  @HostListener('window:click', ['$event']) windowClick($event) {
+     console.log("Ohh on m'a clické dessus !!!",$event);
+   }
+   
+  @HostListener('document:keydown', ['$event'])
+  handleKeydownEvent(event: KeyboardEvent) {
+      console.log(event);
+      this.newColor='green';
 
+      if (event.code == "ArrowUp") {
+        this.newColor='green';
+       }
+      else if (event.code == "ArrowDown") {
+        this.newColor='red';
+      }
+      else if (event.code == "ArrowLeft") {
+        this.newColor='blue';
+      }
+      else if (event.code == "ArrowRight") {
+        this.newColor='orange';
+      }
+  }
+ 
   constructor() {
   }
 
